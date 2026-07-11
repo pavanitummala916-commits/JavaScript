fb812cc43195776f4866064b2ac0752de76362e8
pavanitummala916-commits <pavanitummala916@gmail.com>
2026-07-11 10:51:17 +0530

    Add files via upload

---
+++ rock,paper,scissor.js
@@ -0,0 +1,10 @@
+a = prompt();
+if (a == "rock") {
+  console.log("paper");
+} else if (a == "Paper") {
+  console.log("scissors");
+} else if (a == "scissors") {
+  console.log("rock");
+} else {
+  console.log("Enter correct value");
+}
