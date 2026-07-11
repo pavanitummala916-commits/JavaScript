2378c5284f8673c03c9d5a1084d4577eb67d3d1c
pavanitummala916-commits <pavanitummala916@gmail.com>
2026-07-11 10:49:25 +0530

    Add files via upload

---
+++ even or divi.js
@@ -0,0 +1,12 @@
+a = prompt();
+if (a % 2 == 0) {
+  console.log("even");
+} else {
+  if (a % 3 == 0) {
+    console.log("Yes,it is odd Divisible by 3");
+  } else {
+    console.log("Yes,it is odd Not divisible by 3");
+  }
+}
+
+
