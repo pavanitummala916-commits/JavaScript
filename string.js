80be4790749e70505b175768588261c172fe8a61
pavanitummala916-commits <pavanitummala916@gmail.com>
2026-07-11 10:56:38 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,15 @@
+let s1 = "single quote";
+let s2 = "double quote 's";
+//let s3 = `back tic`;
+let s3 = `back tic ${s1}`;
+
+console.log(typeof s1);
+console.log(typeof s2);
+console.log(typeof s3);
+
+let s1 = "Pavani";
+console.log(s1.toUpperCase());
+console.log(s1.toLowerCase());
+console.log(s1.length);
+console.log(s1[4]);
+console.log(s1[-1]);
