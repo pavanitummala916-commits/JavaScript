7675c5654d068d763e157b5b23c56568c75a5eb4
pavanitummala916-commits <pavanitummala916@gmail.com>
2026-07-11 10:49:52 +0530

    Add files via upload

---
+++ even or odd.js
@@ -0,0 +1,21 @@
+let n = 10;
+if (n % 2 == 0) {
+  console.log("even");
+} else {
+  console.log("odd");
+}
+
+//Dynamic input
+let a = prompt("Enter a value: ");
+if (a % 2 == 0) {
+  console.log("even");
+} else {
+  console.log("odd");
+}
+
+console.log("5" + 10);
+console.log("5" * 10);
+console.log(Number("5") + 10);
+
+
+let a = prompt("Enter ")
