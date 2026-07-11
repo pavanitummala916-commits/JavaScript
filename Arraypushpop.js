36e79e8df3849d12716e37b41b9781c14f030aed
pavanitummala916-commits <pavanitummala916@gmail.com>
2026-07-11 10:52:48 +0530

    Add files via upload

---
+++ Arraypushpop.js
@@ -0,0 +1,16 @@
+let arr = [23, 65, 77546, 54343];
+
+console.log(arr);
+
+arr.push(100);
+console.log(arr);
+
+arr.unshift(111);
+console.log(arr);
+
+let res = arr.pop();
+console.log("poped ele:", res);
+console.log(arr);
+
+let res2 = arr.shift(32434);
+console.log(arr);
