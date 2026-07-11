72c14dbd9885856b2ff283b0461a3da40f91e825
pavanitummala916-commits <pavanitummala916@gmail.com>
2026-07-11 10:56:11 +0530

    Add files via upload

---
+++ slice.js
@@ -0,0 +1,22 @@
+let arr = ["apple", "banna", "iceapple", "papaya", "melon", "apple"];
+new_arr = arr.slice(1, 4);
+console.log(arr);
+console.log(new_arr);
+
+let s = arr.toString();
+console.log(s);
+console.log(typeof s);
+
+//slice(start_idx,end_idx)
+let s1 = "Pavani";
+new_s = s1.slice(2, 5);
+console.log(new_s);
+
+new_s1 = s1.substring(2, 5);
+console.log(new_s1);
+
+
+let s1 = "ABC";
+console.log(s1,at(0));
+console.log(s1.charAt(1));
+console.log(s1,charAt(0));
