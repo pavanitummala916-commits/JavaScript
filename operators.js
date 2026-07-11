9d97ef3c44978b19cf5409cca60d06a22ea39bf6
pavanitummala916-commits <pavanitummala916@gmail.com>
2026-07-11 10:50:45 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,60 @@
+alert("This is Js");
+
+//Arithmetic operators:
+let name = "Pavani";
+console.log("Arithmetic operatos");
+console.log("Addition", 10 + 20);
+console.log("subtraction", 10 - 20);
+console.log("Multiplication", 10 * 20);
+console.log("Division", 60 / 6);
+console.log("modulo", 10 % 20);
+console.log("power", 10 ** 20);
+console.log(`My name is ${name}`);
+
+//Assignment operators:
+let a = 10;
+a += 20;
+console.log("a", a);
+
+//Relational operator
+console.log(10 == "10");
+console.log(10 === "10");
+console.log(10 !== "10");
+
+//logical operators:
+console.log(true && true);
+console.log(true || false);
+console.log(!false);
+
+//Bitwise operators:
+console.log(5 & 3);
+console.log(5 | 3);
+console.log(~5);
+
+//Terinary operator
+a = 5;
+a++;
+console.log(a);
+
+a = 11;
+console.log(++a);
+
+a = 10;
+console.log(--a);
+
+a = 10;
+console.log(a--);
+
+c = "5";
+console.log(typeof c);
+console.log(typeof +c);
+
+//Terinary operator
+name = 10 < 0 ? "Pavani" : "jaya";
+console.log(name);
+
+//nullish operator
+branch = null;
+console.log(branch ?? "CDS");
+
+
